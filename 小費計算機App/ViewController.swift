27fbdf140cf48fab401643d8befc8cbaf53689d1
Robert0321 <shat0806449@gmail.com>
2022-09-15 17:21:06 +0800

    Initial Commit

---
+++ 小費計算機App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  小費計算機App
+//
+//  Created by robert on 2022/9/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
